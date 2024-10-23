7ea4b09b89fb80ab8a74933607fa4f2a1be10c13
EPW <erikpw009@gmail.com>
2024-10-23 15:22:31 -0700

    debugging

---
+++ src/routes/fromDBandComponents/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+	import ContainerTable from '$lib/ContainerTable.svelte';
+	export let data;
+
+	let clickedContainerNumber = "none"
+
+	function containerClicked(container) {
+		console.log('Container clicked:', container.containernumber);
+		if (container) {
+			clickedContainerNumber = container.containernumber
+		}
+	}
+</script>
+
+<h1>Leaning into Svelte's approach: Components for UI elements</h1>
+
+<ContainerTable containers={data.containers} onContainerClicked={containerClicked} />
+
+<span class=i>Last container clicked: {clickedContainerNumber}</span>

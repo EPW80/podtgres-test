40a3343f0ba6b4fa0e0ad27189584db7b7deff72
EPW <erikpw009@gmail.com>
2024-10-23 16:10:43 -0700

    debugging

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/chunks/database.js
+++
@@ -1,6 +0,0 @@
-import postgres from "postgres";
-const PGCONNECT = "postgres://postgres:root@localhost:5432/containers?sslmode=require";
-const sql = postgres(PGCONNECT, {});
-export {
-  sql as s
-};
--- .vercel/output/static/_app/immutable/entry/start.CIX-qLyc.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.B4B0FvmY.js";export{t as start};
